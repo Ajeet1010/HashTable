e495523edb88e718ada644677e5036f7a63292ea
ajeet1010 <ajeetpal101999@gmail.com>
2023-02-16 23:32:01 +0530

    [Ajeet] Frequency of word in a sentence

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace HashTable
+﻿using System.Collections;
+
+namespace HashTable
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*****  Hash Table's Programming  *****");
+            string paragraph = "To be or not to be";
+            CountNumbOfOccurence(paragraph);
+            Console.Write("\nPress any key to continue...... ");
+
+        }
+        
+        public static void CountNumbOfOccurence(string paragraph)
+        {
+            MyMapNode<string, int> hashTabe = new MyMapNode<string, int>(6);
+
+            string[] words = paragraph.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (hashTabe.Exists(word.ToLower()))
+                    hashTabe.Add(word.ToLower(), hashTabe.Get(word.ToLower()) + 1);
+                else
+                    hashTabe.Add(word.ToLower(), 1); //to,1 
+            }
+            Console.WriteLine("Displaying after add operation\n---------------------------------");
+            Console.WriteLine("Word      NumberOfFrequency");
+            hashTabe.Display();
         }
     }
 }
